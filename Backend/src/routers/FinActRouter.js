687c7a132793e004d33bb5da96cb8b405ee56f5b
PiratheepThomas <IT19963334@my.sliit.lk>
2022-04-17 11:16:47 +0530

    update

--- Backend/src/routers/FinActRouter.js
+++ Backend/src/routers/FinActRouter.js
@@ -2,15 +2,13 @@ const express = require('express')
 const app = express.Router();
 const userController = require('../controllers/FinanceController')
 
-//:GET http://localhost:5000/user/
+
 app.get('/',financeController.getActivities);
 
-//:POST http://localhost:5000/user/add
 app.post('/add',financeController.addFinanceActivity);
 
-//:GET http://localhost:5000/user/123
 app.get('/:id',financeController.getActivityById);
 
-//:PUThttp://localhost:5000/user/add
 app.put('/:id',financeController.updateActivity)
+
 module.exports = app;
