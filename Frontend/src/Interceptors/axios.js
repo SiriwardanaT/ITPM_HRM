2cf34e9b427e1273fbfbc1ff050ad2a584ea60cb
SiriwardanaT <thariharshana123@gmail.com>
2022-05-02 12:30:32 +0530

    set the token in request header

--- Frontend/src/Interceptors/axios.js
+++ Frontend/src/Interceptors/axios.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
-
+import authHelper from '../services/authHelpers'
 const ins = axios.create({
     baseURL:'http://localhost:5000/'
 });
 
 ins.interceptors.request.use(config =>{
     console.log(config)
+    config.headers.Authorization = authHelper.getToken();
     return config
 },err =>{
     return Promise.reject(err)
