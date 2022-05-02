0391c0724ee8d531f262c00fdd6e19f048578727
SiriwardanaT <thariharshana123@gmail.com>
2022-05-02 15:21:42 +0530

    authenticate user functions

--- Backend/src/routers/UserRouter.js
+++ Backend/src/routers/UserRouter.js
@@ -7,14 +7,14 @@ const Auth = require('../middleware/authMiddleware')
 app.get('/',Auth.IsAuthenticated,userController.getAllEmployees);
 
 //:POST http://localhost:5000/user/add
-app.post('/add',userController.addEmployee);
+app.post('/add',Auth.IsAuthenticated,userController.addEmployee);
 
 //:GET http://localhost:5000/user/123
-app.get('/:id',userController.getEmployeeById);
+app.get('/:id',Auth.IsAuthenticated,userController.getEmployeeById);
 
 //:GET http://localhost:5000/user/my
 app.get('/profile/my',Auth.IsAuthenticated,userController.getUserProfile)
 
 //:PUThttp://localhost:5000/user/add
-app.put('/:id',userController.updateEmployee)
+app.put('/:id',Auth.IsAuthenticated,userController.updateEmployee)
 module.exports = app;
