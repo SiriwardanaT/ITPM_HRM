1f2c8934a485a63f6318af9853ec0dfcacc6daf7
SiriwardanaT <thariharshana123@gmail.com>
2022-05-06 00:24:18 +0530

    Add new Token constants

--- Backend/src/constants/auth.js
+++ Backend/src/constants/auth.js
@@ -1,7 +1,9 @@
 const constant_for_roles = {
     ADMIN_PRIVILLADGE_ACCEPTED : true,
     ADMIN_PRIVILLADGE_REJECTED : false,
-    TOKEN_KEY                  : "wG8cXT3IQ4wG8cXT3IQ4"
+    TOKEN_KEY                  : "wG8cXT3IQ4wG8cXT3IQ4",
+    ADMIN                      : "Admin",
+    USER                       : "User"
 }
 
 module.exports = constant_for_roles
