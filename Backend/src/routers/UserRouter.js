e1a37e7fe9ffbcd664d4ca41268604c495edeaef
SiriwardanaT <thariharshana123@gmail.com>
2022-05-06 00:26:16 +0530

    assign all routes to validate (user roles)

--- Backend/src/routers/UserRouter.js
+++ Backend/src/routers/UserRouter.js
@@ -2,19 +2,20 @@ const express = require('express')
 const app = express.Router();
 const userController = require('../controllers/UserController')
 const Auth = require('../middleware/authMiddleware')
+const IsGrantAccess = require('../middleware/AccessControll')
 
 //:GET http://localhost:5000/user/
 app.get('/',Auth.IsAuthenticated,userController.getAllEmployees);
 
 //:POST http://localhost:5000/user/add
-app.post('/add',Auth.IsAuthenticated,userController.addEmployee);
+app.post('/add',Auth.IsAuthenticated,IsGrantAccess.AccessController(['Admin']),userController.addEmployee);
 
 //:GET http://localhost:5000/user/123
-app.get('/:id',Auth.IsAuthenticated,userController.getEmployeeById);
+app.get('/:id',Auth.IsAuthenticated,IsGrantAccess.AccessController(['Admin']),userController.getEmployeeById);
 
 //:GET http://localhost:5000/user/my
-app.get('/profile/my',Auth.IsAuthenticated,userController.getUserProfile)
+app.get('/profile/my',Auth.IsAuthenticated,IsGrantAccess.AccessController(['User','Admin']),userController.getUserProfile)
 
 //:PUThttp://localhost:5000/user/add
-app.put('/:id',Auth.IsAuthenticated,userController.updateEmployee)
+app.put('/:id',Auth.IsAuthenticated,IsGrantAccess.AccessController(['Admin','User']),userController.updateEmployee)
 module.exports = app;
