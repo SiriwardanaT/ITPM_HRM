d3704f4e01e9931412e80e51fcfd873e6ced2253
tharukshi724 <tharukshiw@gmail.com>
2022-05-07 20:18:11 +0530

    email part created

---
+++ Backend/src/constants/emails.js
@@ -0,0 +1,8 @@
+const EMAIL_CONSTANSTS ={
+    USER_EMAIL : 'itpm.hrm@gmail.com',
+    USER_PASSWORD : 'Tharu724*'
+} 
+
+module.exports={
+    EMAIL_CONSTANSTS
+}
---
+++ Backend/src/controllers/emailController.js
@@ -0,0 +1,71 @@
+const nodemailer = require('nodemailer');
+const constants_for_email = require('../constants/emails');
+
+const sendEmails =(req,res)=>{
+    const name = req.params.empName;
+    const email = req.params.empEmail;
+    const action = req.body.action;
+
+   const transporter = nodemailer.createTransport({
+     service: 'gmail',
+     auth: {
+       user: constants_for_email.EMAIL_CONSTANSTS.USER_EMAIL,
+       pass: constants_for_email.EMAIL_CONSTANSTS.USER_PASSWORD,
+      
+     }
+   });
+   if(action == 'Approve'){
+    let mailOptions = {
+      from: name,
+      to: email,
+      subject: 'Status of the requested leave',
+      text: `Hi ${name} , 
+      
+             Your leave request is approved by the admin. Enjoy your holiday!!!
+             
+             Thanks and Best Regards.
+             
+             `
+    };
+    transporter.sendMail(mailOptions, function(err, data) {
+      if (err) {
+        console.log("Error " + err);
+      } else {
+        console.log("Email sent successfully");
+      }
+    });
+   }if(action='Reject'){
+    let mailOptions = {
+      from: name,
+      to: email,
+      subject: 'Status of the requested leave',
+      text: `Hi ${name} , 
+      
+            Your leave request is rejected by the admin
+             
+             Thanks and Best Regards.
+             
+             `
+    };
+    transporter.sendMail(mailOptions, function(err, data) {
+      if (err) {
+        console.log("Error " + err);
+      } else {
+        console.log("Email sent successfully");
+      }
+    });
+   }
+   
+    
+    
+
+   
+
+
+
+
+}
+
+module.exports ={
+ sendEmails
+} 
