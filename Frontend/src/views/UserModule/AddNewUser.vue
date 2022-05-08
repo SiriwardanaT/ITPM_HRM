3c8bc6dcb4205b5816cac5005dad94a2121aafbc
SiriwardanaT <thariharshana123@gmail.com>
2022-05-08 21:49:13 +0530

    Add clear button and cancel button

--- Frontend/src/views/UserModule/AddNewUser.vue
+++ Frontend/src/views/UserModule/AddNewUser.vue
@@ -1,31 +1,22 @@
 <template>
   <v-container>
     <v-row>
-        <v-col>
-            <h2 v-if="this.$route.path =='/member/update'">Update Employee</h2>
-            <h2 v-if="this.$route.path =='/member/add'">Add Employee</h2>
-        </v-col>
-        <v-col v-if="this.$route.path =='/member/update'">
-            <v-card style="height:50px" elevation="6">
-                <v-container>
-                  <div>
-                    <v-chip
-                    class=""
-                    color="success"
-                    outlined
-                    pill
-                  >
-                   EID  {{user.employeeId}}
-                    <v-icon right>
-                      mdi-account-outline
-                    </v-icon>
-                  </v-chip>
-                  </div>
-                </v-container>
-               
-            </v-card>
-        </v-col>
-         
+      <v-col>
+        <h2 v-if="this.$route.path == '/member/update'">Update Employee</h2>
+        <h2 v-if="this.$route.path == '/member/add'">Add Employee</h2>
+      </v-col>
+      <v-col v-if="this.$route.path == '/member/update'">
+        <v-card style="height: 50px" elevation="6">
+          <v-container>
+            <div>
+              <v-chip class="" color="success" outlined pill>
+                EID {{ user.employeeId }}
+                <v-icon right> mdi-account-outline </v-icon>
+              </v-chip>
+            </div>
+          </v-container>
+        </v-card>
+      </v-col>
     </v-row>
     <!-- form start -->
     <v-form ref="form">
@@ -107,7 +98,12 @@
             ></v-text-field>
           </v-col>
           <v-col>
-            <v-file-input label="File input" v-model="user.profile_img" outlined dense></v-file-input>
+            <v-file-input
+              label="File input"
+              v-model="user.profile_img"
+              outlined
+              dense
+            ></v-file-input>
           </v-col>
         </v-row>
 
@@ -128,7 +124,7 @@
               </div>
             </v-col>
 
-            <v-col v-if="this.$route.path =='/member/add'">
+            <v-col v-if="this.$route.path == '/member/add'">
               <v-chip style="width: 50%">Password Information</v-chip>
               <div class="mt-3">
                 <v-text-field
@@ -141,21 +137,13 @@
                 ></v-text-field>
               </div>
             </v-col>
-             <v-col v-if="this.$route.path =='/member/update'">
+            <v-col v-if="this.$route.path == '/member/update'">
               <v-chip style="width: 50%">Update Status </v-chip>
               <div class="mt-3">
-                      <v-radio-group v-model="user.status">
-                          <v-radio
-                            label="Active"
-                            value="true"
-                            key="1"
-                          ></v-radio>
-                           <v-radio
-                            key="2"
-                            label="Inactive"
-                            value="false"
-                          ></v-radio>
-                      </v-radio-group>
+                <v-radio-group v-model="user.status">
+                  <v-radio label="Active" value="true" key="1"></v-radio>
+                  <v-radio key="2" label="Inactive" value="false"></v-radio>
+                </v-radio-group>
               </div>
             </v-col>
           </v-row>
@@ -168,8 +156,23 @@
             >
               Save
             </v-btn>
-            <v-btn class="ma-2 white--text" style="width: 10%" color="orange">
+            <v-btn
+              v-if="this.$route.path == '/member/add'"
+              class="ma-2 white--text"
+              style="width: 10%"
+              color="orange"
+              @click="clear()"
+            >
               Clear
+            </v-btn>
+            <v-btn
+              v-if="this.$route.path == '/member/update'"
+              class="ma-2 white--text"
+              style="width: 10%"
+              color="red"
+              @click="cancel()"
+            >
+              Cancel
             </v-btn>
           </div>
         </div>
@@ -177,26 +180,26 @@
     </v-form>
     <Loader v-if="Isloading" />
     <ErrorMsg :msg="Errormsg" v-if="ErrActive" />
-    <SuccessMsg :msg = "Successmsg" v-if="SuccessActive"/>
+    <SuccessMsg :msg="Successmsg" v-if="SuccessActive" />
   </v-container>
 </template>
 
 <script>
 import Loader from "../../components/Notification/Loading.vue";
-import UserAPI from "../../services/userService"
-import ErrorMsg from "../../components/Notification/Error.vue"
-import SuccessMsg from "../../components/Notification/Success.vue"
-import axios from 'axios';
+import UserAPI from "../../services/userService";
+import ErrorMsg from "../../components/Notification/Error.vue";
+import SuccessMsg from "../../components/Notification/Success.vue";
+import axios from "axios";
 export default {
   data() {
     return {
-      title:"",
+      title: "",
       check: "",
       Isloading: true,
-      Errormsg:"Already Created User",
-      ErrActive :false,
-      SuccessActive:false,
-      Successmsg :"Employee Created",
+      Errormsg: "Already Created User",
+      ErrActive: false,
+      SuccessActive: false,
+      Successmsg: "Employee Created",
       GenderItems: ["Male", "Female"],
       JobRole: [
         "Intern",
@@ -233,8 +236,8 @@ export default {
 
       //bind data
       user: {
-        _id:"",
-        employeeId:"",
+        _id: "",
+        employeeId: "",
         employeeName: "",
         phone: "",
         email: "",
@@ -245,85 +248,86 @@ export default {
         gender: "",
         profile_img: "",
         password: "",
-        status:""
+        status: "",
       },
     };
   },
   components: {
     Loader,
     ErrorMsg,
-    SuccessMsg
+    SuccessMsg,
   },
   methods: {
     async login() {
-     
       this.SuccessActive = false;
       this.ErrActive = false;
       this.$refs.form.validate();
       if (this.$refs.form.validate()) {
+        this.Isloading = true;
+        console.log(this.user);
+        if (this.$route.path == "/member/update") {
+          console.log(this.user);
           this.Isloading = true;
-          console.log(this.user)
-          if(this.$route.path =='/member/update'){
-              console.log(this.user);
-              this.Isloading = true;
-              const IsUpdate = await UserAPI.updateEmployee(this.user,this.user._id);
-              if(IsUpdate){
-                  this.Isloading = false;
-                  this.SuccessActive = true;
-                  this.Successmsg = "Updated SuccessFully"
-                  setTimeout(()=>{
-                    this.$router.push('/member');
-                  },2000)
-              }
-              else{
-                  this.ErrActive = true
-                  this.Isloading = false;
-                  this.Errormsg = "No changes found"
-              }
-              
-
+          const IsUpdate = await UserAPI.updateEmployee(
+            this.user,
+            this.user._id
+          );
+          if (IsUpdate) {
+            this.Isloading = false;
+            this.SuccessActive = true;
+            this.Successmsg = "Updated SuccessFully";
+            setTimeout(() => {
+              this.$router.push("/member");
+            }, 2000);
+          } else {
+            this.ErrActive = true;
+            this.Isloading = false;
+            this.Errormsg = "No changes found";
           }
-          else if(this.$route.path =='/member/add'){
-           const fd = new FormData();
-           fd.append("employeeName",this.user.employeeName)
-           fd.append("phone",this.user.phone)
-           fd.append("email",this.user.email)
-           fd.append("Nic",this.user.Nic)
-           fd.append("birthData",this.user.birthData)
-           fd.append("address",this.user.address)
-           fd.append("jobRole",this.user.jobRole)
-           fd.append("gender",this.user.gender)
-           fd.append("password",this.user.password)
-           fd.append("status",this.user.status)
-           fd.append("profile",this.user.profile_img)
-            const IsCreate = await UserAPI.addEmployee(fd);
-            // IsCreate ? this.SuccessActive : this.ErrActive = true
-            if(IsCreate){
-               this.SuccessActive = true;
-               this.Isloading = false;
-               setTimeout(()=>{
-                  this.$router.push('/member');
-               },1500)
-            }
-            else{
-                this.ErrActive = true
-                this.Isloading = false;
-            }
+        } else if (this.$route.path == "/member/add") {
+          const fd = new FormData();
+          fd.append("employeeName", this.user.employeeName);
+          fd.append("phone", this.user.phone);
+          fd.append("email", this.user.email);
+          fd.append("Nic", this.user.Nic);
+          fd.append("birthData", this.user.birthData);
+          fd.append("address", this.user.address);
+          fd.append("jobRole", this.user.jobRole);
+          fd.append("gender", this.user.gender);
+          fd.append("password", this.user.password);
+          fd.append("status", this.user.status);
+          fd.append("profile", this.user.profile_img);
+          const IsCreate = await UserAPI.addEmployee(fd);
+          // IsCreate ? this.SuccessActive : this.ErrActive = true
+          if (IsCreate) {
+            this.SuccessActive = true;
+            this.Isloading = false;
+            setTimeout(() => {
+              this.$router.push("/member");
+            }, 1500);
+          } else {
+            this.ErrActive = true;
+            this.Isloading = false;
           }
-          else{
-             alert("Invalid")
-          }
-          
+        } else {
+          alert("Invalid");
+        }
       }
+    },
+    clear() {
+      this.$refs.form.reset();
+    },
+    cancel() {
+      this.$router.push("/member");
     },
   },
   async created() {
-    setTimeout(async() => {
-      if(this.$route.path =='/member/update'){
-          const UserId = this.$route.query._id
-          const Curruntuser = await UserAPI.getEmployeeById(UserId);
-          this.user = Curruntuser;
-          this.user.birthData = Curruntuser.birthData.split('T')[0]
+    setTimeout(async () => {
+      if (this.$route.path == "/member/update") {
+        const UserId = this.$route.query._id;
+        const Curruntuser = await UserAPI.getEmployeeById(UserId);
+        this.user = Curruntuser;
+        this.user.birthData = Curruntuser.birthData.split("T")[0];
       }
       this.Isloading = false;
     }, 1500);
